116f955664e6e0bb9351b847899a052a6a810d84
tqchung <chung.thanquoc@basenet.ch>
2022-09-14 20:44:27 +0700

    arrray

--- operators.js
+++ operators.js
@@ -104,11 +104,7 @@ for (var demTheKy = 0; demTheKy < 22; demTheKy = demTheKy + 1) {
  */
 
 
-// array: khai báo, truy xuất phần tử.
-// var array01 = [];
-// để truy xuất phần tử -> sử dụng cú pháp [], ví dụ
 
-// console.log(array01[0]); console.log(array01[1]); 
 
 // vị trí sử dụng để truy xuất phần tử của array có thể là một biến, ví dụ
 /**
@@ -121,20 +117,53 @@ for (var demTheKy = 0; demTheKy < 22; demTheKy = demTheKy + 1) {
 /**
  * loop:
  */
+// array: khai báo, truy xuất phần tử.
+// var array01 = [];
+// để truy xuất phần tử -> sử dụng cú pháp [], ví dụ
+var diemSo = [2 , 3]; // 0 -> 1
 
+console.log(diemSo[1]); // -> 3;
+console.log(diemSo[0]); // -> 2;
+diemSo[0] = 10;
+var diemSoCuoiCung = diemSo[1];
+var tong = diemSo[1] + diemSo[2];
+console.log(diemSo.length);
+// console.log(array01[0]); console.log(array01[1]); 
 /*
  * 
- * 1.1 Given an array, calculate the sum of every element.
+ * 1.1 Khai báo môt mảng các số nguyên, tính tổng. 
+ * // vong lap + truy xuat phan tu mang
+ * */
+var mang = [1, 5, 10, 20, 30];
+var tong = mang[0] + mang[1] + mang[2] //+//...;
+
+for (var i = 0; i < 5; ++i) {
+    var tong2 = tong2 + mang[i];
+}
+
+console.log(tong2);
+
+ /*
  * 1.2 Khai báo một số nguyên, tính toán giai thừa.
+
  * 
  * 2. Khai báo một array số nguyên, viết code javascript tìm số lớn nhất. Sử dụng vòng lặp, câu lệnh điều kiện, lệnh gán và lệnh so sánh.
+ *
+ */
+ var mangSosanh = [2, 4, 1, 5, 6, 5];
+ var soLonNhat = 0;
+ for(;;) { // duyet tat ca cac phan tu
+    // so sanh -> neu phan tu hien tai lon hon so lon nhat, thi phan tu hien tai la so lon nhat.
+    if (/** phan tu hien tai lon hon soLonNhat */) {
+        // soLonNhat = phan tu hien tai;
+    }
+ }
+ console.log(soLonNhat);
+ /*
  * 
  * 3. Tìm số chẵn lớn nhất trong một array số nguyên.
- * 
  * 3.1 Cho một array các số nguyên, tính tổng tất cả các số lẻ.
- * 
- * 4. Cho một số nguyên, in ra số chữ số của số đó.
- * 
+ * 4. Cho một số nguyên, đếm số chữ số của số đó. 1000 -> 4. 200 -> 3
  * 5. kiểm tra xem một dãy số nguyên có phải là một dãy đối xứng.
  * 
  */
