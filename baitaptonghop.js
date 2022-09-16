140b06ded49f08591c073e8e9c43b2eeca2602b2
tqchung <chung.thanquoc@basenet.ch>
2022-09-16 21:08:11 +0700

    baitap tong hop

---
+++ baitaptonghop.js
@@ -0,0 +1,20 @@
+/*
+ * 3. Khai báo một array các số nguyên, gọi hàm kiểm tra số nguyên tố, in ra các số nguyên tố ở trong array.
+ * 4. Viết hàm với tham số là một mảng và một số nguyên N. trả về số lần lặp lại của số N.
+ * function ham01(array1, n) {
+ *  
+ * } 
+ * var solanLaplai = ham01([1, 2, 3 ,4, 5, 2, 4], 2);
+ * console.log(solanLaplai);
+ */
+/**
+
+5. Viết một hàm với Tham số là một số nguyên, trả về một array là các chữ số:
+vd: số nguyên 4381
+-> trả về [4, 3, 8, 1]
+
+6. Khai báo một mảng số nguyên, Viết một hàm tăng tất cả phần từ lên một đơn vị.
+vd: input: [10, 20, 30]
+-> trả về [11, 21, 31]
+
+ */
